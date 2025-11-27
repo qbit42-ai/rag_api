7574af33d3c8699dd04c21818402d1534ed56679
Tom <thomas.reulen@gmail.com>
2025-11-27 15:08:53 +0100

    parsing with double quotes

--- container/tools/entrypoint.sh
+++ container/tools/entrypoint.sh
@@ -3,12 +3,12 @@
 
 echo "custome entrypoint..."
 
-AWS_PAGER="" aws secretsmanager get-secret-value --secret-id /config/env --output text --query 'SecretString' >.env.json
+AWS_PAGER="" aws secretsmanager get-secret-value --profile SandboxAdmin --secret-id /config/env --output text --query 'SecretString' >.env.json
 
-jq -r 'to_entries | .[] | "\(.key)=\(.value)"' .env.json >.env
+jq -r 'to_entries | .[] | "\(.key)=\"\(.value)\""' .env.json > .env
 
-set -a 
 # automatically export all variables
+set -a 
 source .env
 set +a
 
